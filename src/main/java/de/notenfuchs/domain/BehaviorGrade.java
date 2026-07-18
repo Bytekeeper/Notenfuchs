67143c5fc71d6fcf46e8d70fb35da896200046f4
Dennis Waldherr <bytekeeper@mailbox.org>
2026-07-18 10:47:17 +0200

    fix: Small import fix for doc

--- src/main/java/de/notenfuchs/domain/BehaviorGrade.java
+++ src/main/java/de/notenfuchs/domain/BehaviorGrade.java
@@ -1,5 +1,6 @@
 package de.notenfuchs.domain;
 
+import de.notenfuchs.service.GradeService;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
